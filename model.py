a40ce5e4f67641c14f092f509accde171647e421
Dixon <styresd@gmail.com>
2018-03-28 11:30:40 -0500

    update

--- model.py
+++ model.py
@@ -1,33 +1,24 @@
 ##Dixon Styres
 ##secuPy
 
-#train and test our models
+#running model
 
 import sklearn
 
 class KNNModel(threading.Thread):
-    """Threaded K Nearest Neighbours Model"""
-    def __init__(self, X, Y, XT, YT, accLabel=None):
+    #test data, test labels, train data, train labels
+    def __init__(self, trainData, trainLabel, testData, testLabel):
         threading.Thread.__init__(self)
-        self.X = X
-        self.Y = Y
-        self.XT=XT
-        self.YT=YT
-        self.accLabel= accLabel
+        self.trainData = trainData
+        self.trainLabel = trainLabel
+        self.testData = testData
+        self.testLabel = testLabel
 
     def run(self):
-        X = np.zeros(self.X.shape)
-        Y = np.zeros(self.Y.shape)
-        XT = np.zeros(self.XT.shape)
-        YT = np.zeros(self.YT.shape)
-        np.copyto(X, self.X)
-        np.copyto(Y, self.Y)
-        np.copyto(XT, self.XT)
-        np.copyto(YT, self.YT)
         for i in range(8):
-            X[:, i] = (X[:, i] - X[:, i].mean()) / (X[:, i].std())
+            self.trainData[:, i] = (self.trainData[:, i] - self.trainData[:, i].mean()) / (self.trainData[:, i].std())
         for i in range(8):
-            XT[:, i] = (XT[:, i] - XT[:, i].mean()) / (XT[:, i].std())
+            self.testData[:, i] = (self.testData[:, i] - self.testData[:, i].mean()) / (self.testData[:, i].std())
         knnModel = KNeighborsClassifier()
         knnModel.fit(X, Y)
         sd = knnModel.predict(XT)
@@ -35,4 +26,3 @@ class KNNModel(threading.Thread):
         acc = (sum(sd == YT) / len(YT) * 100)
         print("Accuracy of KNN Model: %.2f" % acc+' %')
         print('=' * 100)
-        if self.accLabel: self.accLabel.set("Accuracy of KNN Model: %.2f" % (acc)+' %')
--- modelTest.py
+++ modelTest.py
@@ -2,3 +2,26 @@
 ##secuPy
 
 #train and test our models
+import sklearn
+
+class KNNModel(threading.Thread):
+    #test data, test labels, train data, train labels
+    def __init__(self, trainData, trainLabel, testData, testLabel):
+        threading.Thread.__init__(self)
+        self.trainData = trainData
+        self.trainLabel = trainLabel
+        self.testData = testData
+        self.testLabel = testLabel
+
+    def run(self):
+        for i in range(8):
+            self.trainData[:, i] = (self.trainData[:, i] - self.trainData[:, i].mean()) / (self.trainData[:, i].std())
+        for i in range(8):
+            self.testData[:, i] = (self.testData[:, i] - self.testData[:, i].mean()) / (self.testData[:, i].std())
+        knnModel = KNeighborsClassifier()
+        knnModel.fit(X, Y)
+        sd = knnModel.predict(XT)
+        print(YT)
+        acc = (sum(sd == YT) / len(YT) * 100)
+        print("Accuracy of KNN Model: %.2f" % acc+' %')
+        print('=' * 100)
